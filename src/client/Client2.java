adb72a63efb9948dc26cecf76d6b50da296ea623
rhabban <corentin.chupin@gmail.com>
2017-02-28 21:24:24 +0100

    Correction fichiers

--- src/client/Client2.java
+++ src/client/Client2.java
@@ -1,16 +1,12 @@
 package client;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Observable;
 
-import View.Launcher;
 
 /**
  * <b>Client</b> allows a user to send messages to the server.
@@ -25,19 +21,12 @@ public class Client2 extends Observable {
 	private Socket socket = null;
 	private OutputStream outputStream;
 	
-	@Override
-    public void notifyObservers(Object arg) {
-        super.setChanged();
-        super.notifyObservers(arg);
-    }
-	
 	/** Create socket, and receiving thread */
     public void InitSocket(String server, int port) throws IOException {
         socket = new Socket(server, port);
         outputStream = socket.getOutputStream();
 
         Thread receivingThread = new Thread() {
-            @Override
             public void run() {
                 try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -51,6 +40,11 @@ public class Client2 extends Observable {
             }
         };
         receivingThread.start();
+    }
+    
+    public void notifyObservers(Object arg) {
+        super.setChanged();
+        super.notifyObservers(arg);
     }
     
     /** Send a line of text */
--- src/client/ClientThread.java
+++ src/client/ClientThread.java
@@ -7,16 +7,16 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- * 
+ * <b>ClientThread</b>
  * @author Corentin
  */
 
-
-// For every client's connection we call this class
 public class ClientThread extends Thread{
 	private String name = null;
+	
 	private DataInputStream streamIn = null;
 	private PrintStream streamOut = null;
+	
 	private Socket clientSocket = null;
 	private ArrayList<ClientThread> threads = new ArrayList<ClientThread>();
 
@@ -25,80 +25,59 @@ public class ClientThread extends Thread{
 		this.threads = threads;
     }
 
-	public void run() {
+	public synchronized void run() {
 		ArrayList<ClientThread> threads = this.threads;
 
 		try {
 			streamIn = new DataInputStream(clientSocket.getInputStream());
 			streamOut = new PrintStream(clientSocket.getOutputStream());
 			
-
 			streamOut.println("Entrer votre nom :");
-			name = streamIn.readLine().trim();
-
-			streamOut.println("Bonjour " + name + " et bienvenue dans le chat. Pour communiquer avec les utilisateurs, il est nécessaire de se positionner à leur portée");
-			synchronized (this) {
-				// Vérifier si c'est nécessaire
-				/*for(ClientThread thread : threads){
-					if(thread == this){
-						name = name;
-					}
-				}*/
-				
+			name = streamIn.readLine();
+			
+							
 				for(ClientThread thread : threads){
 					if(thread != this){
-						thread.streamOut.println("/* Un nouvel utilisateur s'est connecté : " + name+ "*/");
+						thread.streamOut.println(name + " s'est connecté");
 					} else {
-						thread.streamOut.println("message posté");
+						thread.streamOut.println("Bonjour " + name + " et bienvenue dans le chat. Pour communiquer avec les utilisateurs, il est nécessaire de se positionner à leur portée");
 					}			
 				}
-			}
+			
 			
 			/* Start the conversation. */
 			while (true) {
 				String line = streamIn.readLine();
-				if (line.startsWith("/quit")) {
-					break;
+				if (line.startsWith("/bye")) {
+					disconnect();
+				} else {
+					sendMessage(line);
 				}
-				
-				/* The message is public, broadcast it to all other clients. */
-				synchronized (this) {
-					for(ClientThread thread : threads){
-						thread.streamOut.println("<" + name + "> " + line);
-					}
-				}
-
             }
-			
-			/*synchronized (this) {
-				for (int i = 0; i < maxClientsCount; i++) {
-					if (threads[i] != null && threads[i] != this
-							&& threads[i].clientName != null) {
-						threads[i].os.println("*** The user " + name
-                + " is leaving the chat room !!! ***");
-					}
-				}
-			}*/
-			streamOut.println("/* Bye " + name + " */");
-
-	      /*
-	       * Clean up. Set the current thread variable to null so that a new client
-	       * could be accepted by the server.
-	       */
-			synchronized (this) {
-				for(ClientThread thread : threads){
-					if(thread == this)
-						thread = null;
-				}
+		} catch (IOException e) {
+			e.printStackTrace(); 
+		}
+  }
+	
+	public synchronized void sendMessage(String message){
+		for(ClientThread thread : threads){
+			thread.streamOut.println("<" + name + "> " + message);
+		}
+	}
+	
+	public synchronized void disconnect(){
+		try{
+			for(ClientThread thread : threads){
+				thread.streamOut.println(name + " s'est déconnecté");
+				if(thread == this)
+					thread = null;
 			}
-			/*
-			 * Close the output stream, close the input stream, close the socket.
-			 */
+			
 			streamIn.close();	
 			streamOut.close();
 			clientSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace(); 
 		}
-  }
+	}
 }
